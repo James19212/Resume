85e8fb326583c2b858cbdd58d122f1f59ee41702
James <jamescajamarca81@icloud.com>
2024-02-27 12:35:36 -0500

    Initial Commit

---
+++ gaugeSample/gaugeSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  gaugeSampleApp.swift
+//  gaugeSample
+//
+//  Created by James Cajamarca  on 2/27/24.
+//
+
+import SwiftUI
+
+@main
+struct gaugeSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
